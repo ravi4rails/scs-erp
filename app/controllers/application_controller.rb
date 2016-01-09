48586cf45d63f2c6ccbca7a4767b8cd1805a4c8d
ravi4rails <ravionrails@gmailcom>
2016-01-09 13:18:00 +0530

    Quick fix.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  before_action :authenticate_user!
-  before_action :authenticate_student!
+  # before_action :authenticate_user!
+  # before_action :authenticate_student!
   protect_from_forgery with: :exception
 end
